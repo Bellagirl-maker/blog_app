2781ded6e8abb526e28e541605e36191e3400647
Isabella <isabellaotoo25@gmail.com>
2023-07-21 09:50:42 +0000

    Update Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :comment_entries, dependent: :destroy
 
   validates :author, presence: true
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
--- db/migrate/20230719110253_create_comment_entries.rb
+++ db/migrate/20230719110253_create_comment_entries.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CreateCommentEntries < ActiveRecord::Migration[6.1]
   def change
     create_table :comment_entries do |t|
