2d4024c1f8182c62cadc71c95580995c95ab6b22
Isabella <isabellaotoo25@gmail.com>
2023-07-24 17:22:28 +0000

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,18 +9,3 @@ class PostsController < ApplicationController
 end
 
 
-# # app/controllers/posts_controller.rb
-# class PostsController < ApplicationController
-#     # The index action for all posts by a given user page
-#     def index_by_user
-#       # Fetch all posts by the given user and store them in an instance variable
-#       @user = User.find(params[:user_id])
-#       @posts = @user.posts
-#     end
-
-#     # The show action for single post page
-#     def show
-#       # Fetch the post by ID and store it in an instance variable
-#       @post = Post.find(params[:id])
-#     end
-#   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,21 +10,3 @@ end
 
 
 
-# # app/controllers/users_controller.rb
-# class UsersController < ApplicationController
-#   def users
-#     # Fetch all users from the database (assuming User model exists)
-#     @users = User.all
-#   end
-
-#   def user
-#     # Fetch the user by id from the database (assuming User model exists)
-#     @user = User.find(params[:id])
-#   end
-
-#   def all_posts_by_user
-#     # Fetch all posts by a given user (assuming there's a Post model with a 'user_id' column)
-#     @user = User.find(params[:user_id])
-#     @posts = @user.posts
-#   end
-# end
